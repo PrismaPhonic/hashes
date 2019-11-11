38b4ecc760f5bb31ce28c4c655c63877b686641d
Peter Farr <Peter@PrismaPhonic.com>
2019-11-10 21:12:52 -0800

    Added experimental methods to make prefix checking more efficient

--- sha1/src/lib.rs
+++ sha1/src/lib.rs
@@ -56,6 +56,8 @@ use digest::generic_array::GenericArray;
 use digest::generic_array::typenum::{U20, U64};
 use block_buffer::BlockBuffer;
 use block_buffer::byteorder::{BE, ByteOrder};
+use std::os::raw::c_int;
+use std::vec::Vec;
 
 mod consts;
 use consts::{STATE_LEN, H};
@@ -70,7 +72,7 @@ pub struct Sha1 {
 
 impl Default for Sha1 {
     fn default() -> Self {
-        Sha1{ h: H, len: 0u64, buffer: Default::default() }
+        Sha1{ h: H, len: 0u64, buffer: Default::default()}
     }
 }
 
@@ -109,6 +111,75 @@ impl Reset for Sha1 {
         self.len = 0;
         self.buffer.reset();
     }
+}
+
+impl Sha1 {
+    pub fn get_result(&mut self) -> GenericArray<u8, U20> {
+        {
+            let state = &mut self.h;
+            let l = self.len << 3;
+            self.buffer.len64_padding::<BE, _>(l, |d| compress(state, d));
+        }
+        let mut out = GenericArray::default();
+        BE::write_u32_into(&self.h,&mut out);
+        out
+    }
+
+    pub fn digest_starts_with(&mut self, prefix: &Vec<u8>, has_odd_length: bool) -> bool {
+        // Add padding to finalize last block.
+        {
+            let state = &mut self.h;
+            let l = self.len << 3;
+            self.buffer.len64_padding::<BE, _>(l, |d| compress(state, d));
+        }
+
+        let byte_len = prefix.len();
+
+        // Check that state starts with prefix
+        // TODO: Make this actually use the length;
+        let partial_sum = self.partial_sum(byte_len);
+
+        // even length case
+        if !has_odd_length {
+            return unsafe {
+                memcmp(partial_sum.as_ptr(), prefix.as_ptr(), byte_len) == 0
+            }
+        }
+
+        // odd length case. we memcmp up to max even length byte, and then check only upper nibble of
+        // last byte
+        if unsafe {
+            memcmp(partial_sum.as_ptr(), prefix.as_ptr(), byte_len-1) != 0
+        } {
+            return false;
+        }
+
+        if prefix[byte_len - 1] >> 4 != partial_sum[byte_len - 1] >> 4 {
+            return false;
+        }
+
+        true
+    }
+
+    // Sum only up to a fixed length.  Used for prefix checking.
+    fn partial_sum(&mut self, len: usize) -> [u8; 20] {
+        let mut digest = [0; 20];
+        digest[0] = (self.h[0] >> 24) as u8;
+        digest[1] = (self.h[0] >> 16) as u8;
+        digest[2] = (self.h[0] >> 8) as u8;
+        digest[3] = (self.h[0] >> 0) as u8;
+        digest
+    }
+}
+
+extern {
+    /// Calls implementation provided memcmp.
+    ///
+    /// Interprets the data as u8.
+    ///
+    /// Returns 0 for equal, < 0 for less than and > 0 for greater
+    /// than.
+    fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> c_int;
 }
 
 impl_opaque_debug!(Sha1);
